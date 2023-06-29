45b4334385ca5fc304188863fb80c9f88c2de491
Avinash Mamidi <avinashmamidi.548@gmail.com>
2023-06-30 00:11:59 +0530

    new

---
+++ scripts/install-jenkins.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+sudo su
+yum update –y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+amazon-linux-extras install epel -y
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+systemctl enable jenkins
+systemctl start jenkins
+
+# Installing Git
+yum install git -y
+
+# Installing Ansible
+amazon-linux-extras install ansible2 -y
+yum install python-pip -y
+pip install boto3
+
+# Provisioning Ansible Deployer Access
+useradd ansibleadmin
+echo ansibleadmin | passwd ansibleadmin --stdin
+sed -i "s/.*#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
+sed -i "s/.*#enable_plugins = host_list, virtualbox, yaml, constructed/enable_plugins = aws_ec2/g" /etc/ansible/ansible.cfg
+ansible-galaxy collection install amazon.aws
+
+# Enable Password Authentication and Grant Sudo Privilege
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+echo "ansibleadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Apache Maven Installation/Config
+#sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+#sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+#sudo yum install -y apache-maven
+#sudo yum install java-1.8.0-devel
+
+#sudo /usr/sbin/alternatives --config java
+#sudo /usr/sbin/alternatives --config javac
+
+# Use The Amazon Linux 2 AMI When Launching The Jenkins VM/EC2 Instance
+# Instance Type: t2.medium or small minimum
+# Open Port (Security Group): 8080
